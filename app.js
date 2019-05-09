c12e705a7958fbc90c2ee2cdec32cdf53a228f8c
seohyundeock <racefl93@likelion.org>
2019-05-09 20:44:47 +0900

    Create readme.md

---
+++ app.js
@@ -0,0 +1,75 @@
+import express from "express";
+import graphqlHttp from "express-graphql";
+import {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLList,
+  GraphQLSchema
+} from "graphql";
+
+// const schema = buildSchema(`
+//   type Query {
+//     hello: String
+//     person: [Person]
+//   }
+
+//   type Person {
+//     name: String
+//     age: Int
+//   }
+// `);
+
+// const root = {
+//   hello: () => "hello",
+//   person: () => {
+//     return [
+//       { name: "seo", age: 27 },
+//       { name: "eun", age: 25 },
+//       { name: "song", age: 17 }
+//     ];
+//   }
+// };
+
+const personType = new GraphQLObjectType({
+  name: "Person",
+  fields: {
+    name: { type: GraphQLString },
+    age: { type: GraphQLInt }
+  }
+});
+
+const queryType = new GraphQLObjectType({
+  name: "Query",
+  fields: {
+    hello: {
+      type: GraphQLString,
+      resolve: () => "hello"
+    },
+    persons: {
+      type: new GraphQLList(personType),
+      resolve: () => {
+        return [
+          { name: "seo", age: 27 },
+          { name: "eun", age: 25 },
+          { name: "song", age: 17 }
+        ];
+      }
+    }
+  }
+});
+
+const schema = new GraphQLSchema({ query: queryType });
+
+const app = express();
+
+app.use(
+  "/",
+  graphqlHttp({
+    schema,
+    // rootValue: root,
+    graphiql: true
+  })
+);
+
+app.listen(3000, () => console.log("Server is running"));
